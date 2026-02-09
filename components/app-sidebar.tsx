474f949575f5cd9d11041c3eadd19ce02d8d3e15
leandro-guimaraes <contatotech47@gmail.com>
2026-02-09 00:45:24 -0300

    Remoção de importações não usadas

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -14,10 +14,8 @@ import {
   IconUsers,
 } from "@tabler/icons-react"
 
-import { NavDocuments } from "@/components/nav-documents"
 import { NavMain } from "@/components/nav-main"
 import { NavSecondary } from "@/components/nav-secondary"
-import { NavUser } from "@/components/nav-user"
 import {
   Sidebar,
   SidebarContent,
